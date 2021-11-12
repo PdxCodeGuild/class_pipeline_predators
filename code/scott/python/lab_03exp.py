1367e9591bd606aad840e44a8b6921a396de4eaf
Scott <scott.henry.moore@gmail.com>
2021-11-11 19:36:23 -0800

    adding lab 3 with exception

---
+++ code/scott/python/lab_03exp.py
@@ -0,0 +1,20 @@
+print()
+# added exception checking for letters or number out of range
+while True:
+    try:
+        score = int(input('Please enter your test score (0-100): '))
+        score = score(range(0, 100))
+        break
+    except: print('please try again')
+if score >= 90:
+    grade = 'A'
+elif score >= 80 and score <= 89:
+    grade = 'B'
+elif score >= 70 and score <= 79:
+    grade = 'C'
+elif score >= 60 and score <= 69:
+    grade = 'D'
+else:
+    grade = 'F'
+print(f'Your letter grade is {grade}')
+print()
