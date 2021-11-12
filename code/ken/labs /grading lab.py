2e531ed9c81c351d8d8763e573a12aa98b6384ff
ken <kenandraewheatt@Kenandraes-Mac-mini.local>
2021-11-11 21:04:14 -0800

    making progress with making change

--- code/ken/labs /grading lab.py
+++ code/ken/labs /grading lab.py
@@ -20,7 +20,16 @@ Numeric Ranges:
 
 Find the specific letter grade (A+, B-, etc). You can check for more specific ranges using `if` statements, or use modulus `%` to get the ones-digit to set another string to `'+'`, `'-'`, or `' '`. Then you can concatenate that string with your grade string. 
 '''
-grade = 88
+
+while True:
+    try:
+        x = int(input('enter a number: '))
+        break
+    except ValueError:
+        print('try again')
+print(x)
+
+grade = x
 
 if grade < 60:
     print('ya got a F')
