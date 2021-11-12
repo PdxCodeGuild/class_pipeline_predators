d26536e7e8d8321fa9bf408c8f3f21202008a388
Ldb2g3 <93901964+Ldb2g3@users.noreply.github.com>
2021-11-11 22:23:37 -0500

    lab 3 ready for review

---
+++ code/LarryWright/grading_3.py
@@ -0,0 +1,24 @@
+# Part 1 
+number = input('Enter a grade')
+
+number = int(number)
+
+print(number)
+
+
+if number >= 90:
+    print("Your grade is an A")
+
+elif number >= 80:
+    print("Your grade is a B")
+
+elif number >= 70:
+    print("Your grade is a C")
+
+elif number >= 60:
+    print("Your grade is a D")
+
+else:
+    print("Your grade is an F")
+
+# Part 2
