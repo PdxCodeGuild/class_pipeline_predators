6beab3e95276955426e347c8f849fc0df8ab04e6
ken <kenandraewheatt@Kenandraes-Mac-mini.local>
2021-11-12 21:55:28 -0800

    make_change_logic_progress

--- code/ken/labs /makechange.py
+++ code/ken/labs /makechange.py
@@ -1,31 +1,31 @@
 # # Make Change
 
-# Let's convert a dollar amount into a number of coins. The input will be the total amount, the output will be the number of quarters, dimes, nickles, and pennies. Always break the total into the highest coin value first, resulting in the fewest amount of coins. First convert the dollar amount (1.36) into the total number of pennies (136), then use floor division `//`, which throws away the remainder. `10/3` is `3.333333`, `10//3` is `3`. 
+    # Let's convert a dollar amount into a number of coins. The input will be the total amount, the output will be the number of quarters, dimes, nickles, and pennies. Always break the total into the highest coin value first, resulting in the fewest amount of coins. First convert the dollar amount (1.36) into the total number of pennies (136), then use floor division `//`, which throws away the remainder. `10/3` is `3.333333`, `10//3` is `3`. 
 
 
-# ```
-# Enter a dollar amount: 1.36
-# 5 quarters, 1 dime, and 1 penny
-# ```
-# ```
-# Enter a dollar amount: 0.67
-# 2 quarters, 1 dime, 1 nickel, 2 pennies
-# ```
+    # ```
+    # Enter a dollar amount: 1.36
+    # 5 quarters, 1 dime, and 1 penny
+    # ```
+    # ```
+    # Enter a dollar amount: 0.67
+    # 2 quarters, 1 dime, 1 nickel, 2 pennies
+    # ```
 
-# ## Version 2 (optional)
+    # ## Version 2 (optional)
 
-# Instead of hard-coding the coins, store them in a list of tuples. This way you can make custom coins.
+    # Instead of hard-coding the coins, store them in a list of tuples. This way you can make custom coins.
 
-'''
-python
-coins = [
-    ('half-dollar', 50),
-    ('quarter', 25),
-    ('dime', 10),
-    ('nickel', 5),
-    ('penny', 1)
-]
-'''
+    # '''
+    # python
+    # coins = [
+    #     ('half-dollar', 50),
+    #     ('quarter', 25),
+    #     ('dime', 10),
+    #     ('nickel', 5),
+    #     ('penny', 1)
+    # ]
+    # '''
 
 # then use a try exception to make sure they enter a float 
 while True:
@@ -38,23 +38,77 @@ print(dollar_amount)
 
 # Make a dictionary for coins 
 coins  = {
-    'quarters': 25, 
-    'dime': 10, 
-    'penny': 1
-    }
+    'quarters': .25, 
+    'dime': .10,
+    'nickels': .05,
+    'penny': .01
+    }                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                   
 
-# then break down amount from position in number?....
-# can you index a float
-# look into modulas 
-#may if statement
-for i in range(20):
-    if i%2 == 1:
-        continue
-    print(i, end=' ')
+change_quarters = dollar_amount // coins['quarters']
+print(change_quarters)
+remainder_quarters = dollar_amount - (change_quarters * coins['quarters']) 
+print(remainder_quarters) 
+change_dimes = remainder_quarters // coins['dime']
+print(change_dimes)
+remainder_dimes = dollar_amount - ((change_quarters * coins['quarters']) + (change_dimes * coins['dime']))
+print(remainder_dimes)
 
-# Return quarters variable and so on 
 
-return quarters 
-return dime 
-return nickel 
-return pennies 
+# print(f'{change_quarters} + quarters + {change_dimes} + dimes + {change_nickels} + pennies + {change_pennies} pennies')
+
+# We have a float
+# in order to get the change we will divide by quarters first 
+
+change = 0 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# control = 0
+
+# for money_point in dollar_amount:
+
+    #    if money_point.isnumeric() == False : 
+    #        continue 
+    #    elif money_point.isnumeric() == True:
+        
+    # print(money_point)
+
+
+# while control < c
+    # then break down amount from position in number?....
+    # can you index a float
+    # look into modulas 
+    #may if statement
+    # for i in range(20):
+    #     if i%2 == 1:
+    #         continue
+    #     print(i, end=' ')
+
+    # Return quarters variable and so on 
+    # You can index a list 
+    # return quarters 
+    # return dime 
+    # return nickel 
+    # return pennies 
+
+    # Make it a list 
