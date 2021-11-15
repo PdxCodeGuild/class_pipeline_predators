7e9b87c5a074b83a5490d2eb7aa0e7cabb2d7d98
Ldb2g3 <93901964+Ldb2g3@users.noreply.github.com>
2021-11-15 17:57:51 -0500

    Update make_change.py

--- code/LarryWright/make_change.py
+++ code/LarryWright/make_change.py
@@ -3,17 +3,29 @@ quarter = .25
 dime = .10
 penny = 0.01
 
-penny_results = total_amount // penny
-dime_results = total_amount // dime
+#factoring in quarters
 quarters_results = total_amount // quarter
-
-
-print(quarters_results)
 
 quarter_change = quarters_results * quarter
 
-remainder_1 = total_amount - quarter_change 
-print(remainder_1)
+remainder_1 = total_amount - quarter_change
 
 
+#factoring in dimes 
+dime_results = round(remainder_1, 2) // dime
 
+dime_change = dime_results * dime
+
+remainder_2 = round(remainder_1, 2) - dime_change
+
+
+#factoring remainder in pennies
+penny_results = round(remainder_2, 2) // penny
+
+penny_change = penny_results * penny
+
+remainder_3 = round(remainder_2, 2) - penny_change
+
+
+results = f' You have {quarters_results} quarters, ' + f'{dime_results} dime, and' + f' {penny_results} penny'
+print(results)
