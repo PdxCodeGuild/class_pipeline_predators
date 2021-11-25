876fa19b272d2455eac2957633f7dce4bacb5542
Ldb2g3 <93901964+Ldb2g3@users.noreply.github.com>
2021-11-25 00:15:17 -0500

    new updates

--- code/LarryWright/countwords.py
+++ code/LarryWright/countwords.py
@@ -1,6 +1,14 @@
 # Count words (Dictionary)
+import string
+import random
+
 filename = 'document.txt'
+lines = [line.lower() for line in filename]
 
 with open('document.txt', 'r', encoding='utf-8') as file:
     text = file.read()
-print(text)
+    words = text.split()
+    table = str.maketrans("", "", string.punctuation)
+    stripped = [w.translate(table) for w in words]
+    print(stripped)
+ 
