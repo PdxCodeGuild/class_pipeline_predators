297a8ecdf12835ff4ed232352fadb8ec6e4bb2e3
ken <kenandraewheatt@Kenandraes-Mac-mini.local>
2022-01-19 21:09:23 -0800

    adding todoapp folder

---
+++ code/ken/ToDo/notes.py
@@ -0,0 +1,12 @@
+# Todo App 
+#---------------- Build------------
+#started env
+#installed djangodjan
+#Need to app and project
+#django-admin startproject todo_project
+#added project now add app
+#cd into folder 
+#python3 manage.py startapp todo_app
+
+
+
