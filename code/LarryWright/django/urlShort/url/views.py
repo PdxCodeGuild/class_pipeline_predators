cd707ceb2ddb072d9a9f9cc62981cad280f8885a
Ldb2g3 <93901964+Ldb2g3@users.noreply.github.com>
2022-02-01 23:17:47 -0500

    updates

---
+++ code/LarryWright/django/urlShort/url/views.py
@@ -0,0 +1,32 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+
+def index(request):
+    return HttpResponse("Hello World")
+
+
+def urlShort(request):
+    if request.method == 'POST':
+        form = Url(request.POST)
+        if form.is_valid():
+            slug = ''.join(random.choice(string.ascii_letters)
+                           for x in range(10))
+            url = form.cleaned_data["url"]
+            new_url = UrlData(url=url, slug=slug)
+            new_url.save()
+            request.user.urlshort.add(new_url)
+            return redirect('/')
+    else:
+        form = Url()
+    data = UrlData.objects.all()
+    context = {
+        'form': form,
+        'data': data
+    }
+    return render(request, 'index.html', context)
+
+
+def urlRedirect(request, slugs):
+    data = UrlData.objects.get(slug=slugs)
+    return redirect(data.url)
