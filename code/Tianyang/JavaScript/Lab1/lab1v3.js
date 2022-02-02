82f203aa6ed2744ee6b9194ee2c829489967b561
Tianyang Zhou <zhouty223@gmail.com>
2022-02-02 00:35:15 -0500

    adding lab1 unit convertor

---
+++ code/Tianyang/JavaScript/Lab1/lab1v3.js
@@ -0,0 +1,12 @@
+const q1 = prompt("what is the distance?")
+const q2= prompt("what are the units?")
+const conv = {
+    ft : 0.3048,
+    mi : 1609.34,
+    m : 1,
+    km : 1000
+}
+conv.yard = 0.9144
+conv.inch = 0.0254
+let ans = q1 + ' '+q2 + ' is ' +q1*conv[q2] +' m'
+console.log(ans)
