ad9d532e1c6a44d2060dbac805431c90634531e5
ken <kenandraewheatt@Kenandraes-Mac-mini.local>
2022-02-25 21:18:20 -0800

    went with less ambitious django solution

--- code/ken/capstone/DBgolfapp_project/DBgolfapp_project/asgi.py
+++ code/ken/capstone/DBgolfapp_project/DBgolfapp_project/asgi.py
@@ -1,16 +1,16 @@
 """
-ASGI config for discgolfnewsproject project.
+ASGI config for DBgolfapp_project project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
 For more information on this file, see
-https://docs.djangoproject.com/en/3.0/howto/deployment/asgi/
+https://docs.djangoproject.com/en/4.0/howto/deployment/asgi/
 """
 
 import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'discgolfnewsproject.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'DBgolfapp_project.settings')
 
 application = get_asgi_application()
