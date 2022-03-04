dd91f20e5bbc0cd4e0b287b4d0ab4509b92e03c8
ken <kenandraewheatt@Kenandraes-Mac-mini.local>
2022-03-03 21:01:59 -0800

    hooked up templates

--- code/ken/capstone/DBgolfapp_project/fantasygolf/views.py
+++ code/ken/capstone/DBgolfapp_project/fantasygolf/views.py
@@ -1,5 +1,6 @@
 from django.http import HttpResponse
+from django.shortcuts import render
 
 
 def index(request):
-    return HttpResponse("Hello, world. Fantasy golf works")
+    return render(request, 'index.html')
